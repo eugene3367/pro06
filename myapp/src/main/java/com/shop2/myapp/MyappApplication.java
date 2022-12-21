fb9da3f9c7de1a21674631325c492b0d9e7b403e
eugene3367 <dbwls2518@naver.com>
2022-12-21 18:05:00 +0900

    pro06

---
+++ myapp/src/main/java/com/shop2/myapp/MyappApplication.java
@@ -0,0 +1,24 @@
+package com.shop2.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappApplication.class, args);
+	}
+	
+//	@Bean
+//	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+//        sessionFactory.setDataSource(dataSource);
+//		
+//        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*Mapper.xml");
+//        sessionFactory.setMapperLocations(res);
+//        
+//        return sessionFactory.getObject();
+//	}
+
+}
